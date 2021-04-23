969b77add4fd331e0f38259577d3062f95fd04b2
Mohammad Basheer <alkiswani.moho@gmail.com>
2021-04-24 00:33:58 +0300

    problem5 fixed finally without using + and *

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -107,11 +107,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  var P2=1;
+  P2 = multiply(multArr[0],multArr[1])[0];
+  P2 = multiply(P2,multArr[2])[0];
+   return [P2,'The numbers ' + multArr + ' have a product of ' + P2 + '.'];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
