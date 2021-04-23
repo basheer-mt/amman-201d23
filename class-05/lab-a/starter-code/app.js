4986a726a012ddef3e13bd37285378a98bde16da
Mohammad Basheer <alkiswani.moho@gmail.com>
2021-04-24 00:29:02 +0300

    problem3 fixed finally without using + and *

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -54,11 +54,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let S = sum(a,b)[0];
+  S = sum(S, c)[0];
+  let P = multiply(a,b)[0];
+  P = multiply(P, c)[0];
+  return [S, P, a +' and ' + b + ' and ' + c + ' sum to ' + S + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + P + '.'];
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
